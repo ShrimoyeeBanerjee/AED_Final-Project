98c98ecde10ce79ffeddf93b56310f1fc0c4196b
ShrimoyeeBanerjee <banerjee.sh@northeastern.edu>
2022-12-10 16:31:11 -0500

    added crop info directory class

--- AEDfinalproject/src/Info/CropInfoDirectory.java
+++ AEDfinalproject/src/Info/CropInfoDirectory.java
@@ -4,10 +4,32 @@
  */
 package Info;
 
+import java.util.ArrayList;
 /**
  *
  * @author Shrimoyee
  */
 public class CropInfoDirectory {
+    
+    private ArrayList<CropInfo> cropDataList;
+
+    public CropInfoDirectory() {
+        this.cropDataList = new ArrayList<>();
+    }
+
+    public ArrayList<CropInfo> getCropDataList() {
+        return cropDataList;
+    }
+
+    public void setCropDataList(ArrayList<CropInfo> cropDataList) {
+        this.cropDataList = cropDataList;
+    }
+    
+    
+    public CropInfo addCropData(){
+        CropInfo cropData = new CropInfo();
+        cropDataList.add(cropData);
+        return cropData;
+    }
     
 }
