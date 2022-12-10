65964417a09be71388ea8f0232e5fdd5afd107e2
ShrimoyeeBanerjee <banerjee.sh@northeastern.edu>
2022-12-10 16:35:26 -0500

    added crop method directory

--- AEDfinalproject/src/Info/CropMethodDirectory.java
+++ AEDfinalproject/src/Info/CropMethodDirectory.java
@@ -4,10 +4,31 @@
  */
 package Info;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Shrimoyee
  */
 public class CropMethodDirectory {
     
+     private ArrayList<CropMethod> cropMethodList;
+
+    public CropMethodDirectory() {
+        this.cropMethodList = new ArrayList<>();
+    }
+
+    public ArrayList<CropMethod> getCropMethodList() {
+        return cropMethodList;
+    }
+
+    public void setCropMethodList(ArrayList<CropMethod> cropMethodList) {
+        this.cropMethodList = cropMethodList;
+    }
+    
+    public CropMethod addCropMethod(){
+        CropMethod cropMethod = new CropMethod();
+        cropMethodList.add(cropMethod);
+        return cropMethod;
+    }
 }
